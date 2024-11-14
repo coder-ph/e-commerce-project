9596e4f361c6d4535e79f1372fab00fe7eaab0e6
Phelix <phelixmbani@gmail.com>
2024-11-14 12:01:22 +0300

    form lable-styling

--- src/Pages/Promo.jsx
+++ src/Pages/Promo.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import NavBar from './NavBar';
 import "../../src/App.css";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 
 function Promo() {
   return (
@@ -10,23 +12,38 @@ function Promo() {
       </header>
       <main className="shadow-lg p-3 mb-5 bg-body rounded promo">
         <h1>Sign up for Promo Deals!!</h1>
-        <div className='login'>
+        <div className="login">
           <form>
-            <label htmlFor="">Contact</label>
+            <label
+              className="col-sm-2 col-form-label col-form-label-md"
+              htmlFor=""
+            >
+              Contact
+            </label>
             <input
               type="tel"
               name="telephone"
               placeholder="Mobile number +254..."
             />{" "}
             <br /> <br />
-            <label htmlFor="">Email </label>
+            <label
+              className="col-sm-2 col-form-label col-form-label-md"
+              htmlFor=""
+            >
+              Email{" "}
+            </label>
             <input
               type="email"
               name="email"
               placeholder="Enter a valid email"
             />{" "}
             <br /> <br />
-            <label htmlFor="">Adress </label>
+            <label
+              className="col-sm-2 col-form-label col-form-label-md"
+              htmlFor=""
+            >
+              Adress{" "}
+            </label>
             <input
               type="adress"
               name="adress"
